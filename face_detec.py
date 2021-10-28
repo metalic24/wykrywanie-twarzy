afed4d59ac4791a0cdae5a1b96ca1145be08504d
metalic24 <65673563+metalic24@users.noreply.github.com>
2021-10-28 08:28:20 +0200

    działa tło

---
+++ face_detec.py
@@ -0,0 +1,80 @@
+
+
+import cv2
+import face_recognition
+from PIL import Image
+import numpy as np
+
+
+def camera():
+    cap = cv2.VideoCapture(0)
+    if not cap.isOpened():
+        print("Cannot open camera")
+        exit()
+
+
+    while True:
+        # Capture frame-by-frame
+        ret, frame = cap.read()
+        rgb_frame = frame[:, :, ::-1]
+        face_locations = face_recognition.face_locations(rgb_frame)
+        if not ret:
+            print("Can't receive frame (stream end?). Exiting ...")
+            break
+
+        img = cv2.imread("C:/Users/Mati/Pictures/myszka.png",-1)
+        for top, right, bottom, left in face_locations:
+
+            a=(right-left)
+            b=(bottom-top)
+            img =  cv2.resize(img, (a, b))
+
+            #usunięcie kanału alpha
+            img = cv2.cvtColor(img, cv2.COLOR_BGR2RGBA)
+
+            alpha_img = img[:, :, 3] / 255.0
+            alpha_frame = 1.0 - alpha_img
+            for c in range(0, 3):
+                frame[top:bottom, left:right, c] = (alpha_img * img[:, :, c] +
+                                                    alpha_frame * frame[top:bottom, left:right, c])
+
+        cv2.imshow('Video', frame)
+
+
+        # 'q' zamyka kamerę
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+    cv2.destroyAllWindows()
+    cap.release()
+
+
+def zdjecie(path):
+
+
+    image = cv2.imread(path)
+    face_locations = face_recognition.face_locations(image)
+    image2 = cv2.imread("C:/Users/Mati/Pictures/myszka.png",-1)
+
+    for top, right, bottom, left in face_locations:
+        # cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
+        img = cv2.resize(image2, ((right - left), (bottom - top)))
+
+
+        a = (right - left)
+        b = (bottom - top)
+
+        img = cv2.cvtColor(img,cv2.COLOR_BGR2RGBA)
+        alpha_img = img[:, :, 3] / 255.0
+        alpha_image = 1.0 - alpha_img
+
+        for c in range(0, 3):
+                    image[top:bottom, left:right, c] = (alpha_img * img[:, :, c] +
+                                                   alpha_image * image[top:bottom, left:right, c])
+
+
+    return image
+
+
+
+
+
---
+++ gui.py
@@ -0,0 +1,48 @@
+import tkinter
+import cv2
+import numpy as np
+import face_detec
+from tkinter import *
+from tkinter import filedialog
+from PIL import Image, ImageTk
+
+
+def w_zdjecie():
+    path = filedialog.askopenfilename()
+    img = face_detec.zdjecie(path)
+
+    cv2.imshow("zamienione", img)
+
+
+    window2 = Tk()
+
+    label2 = tkinter.Label(window2,text="Czy chcesz zapisać obraz?")
+    label2.pack()
+    #todo zapisz
+    #zapis = tkinter.Button(window2, text="zapisz",command= lambda zapisz()).pack()
+
+
+
+    window2.mainloop()
+
+
+
+
+
+window = Tk()
+window.geometry("480x320")
+window.title("Projekt 1")
+label1 = tkinter.Label(window, text="Podmiana Twarzy")
+label1.pack()
+button_camera = Button(window, text="Zamiana twarzy live", command=face_detec.camera)
+button_camera.pack()
+
+button_zdjecie = Button(window, text="Zamiana twarzy ze zdjecia", command=w_zdjecie)
+button_zdjecie.pack()
+
+label2 = tkinter.Label(window, text="Neuronowy transfer stylów ")
+label2.pack()
+button_neuronowy = Button(window, text="Załaduj zdjęcia")
+button_neuronowy.pack()
+
+window.mainloop()
